704747de0ffced15a4f8c136dea245d3d737d2d9
akim2602 <akim2602@thetis.students.cs.ubc.ca>
2022-10-30 20:01:11 -0700

    Restructured the project

--- resources/ddl.sql
+++ resources/ddl.sql
@@ -1,42 +1,40 @@
+CREATE TABLE Branch(
+    City CHAR(40),
+    StreetAddress CHAR(60),
+    PRIMARY KEY (City, StreetAddress)
+);
+
 CREATE TABLE StaffRuns(
     AreaOfExpertise CHAR(30),
     Name CHAR(40) NOT NULL,
     IDNumber INTEGER PRIMARY KEY,
     BranchCity CHAR(40) NOT NULL,
     BranchStreetAddress CHAR(60) NOT NULL,
-    FOREIGN KEY(BranchCity, BranchStreetAdress),
-    REFERENCES Branch(City, StreetAddress) ON DELETE CASCADE ON UPDATE CASCADE
+    FOREIGN KEY(BranchCity, BranchStreetAddress) REFERENCES Branch(City, StreetAddress) ON DELETE CASCADE
 );
 CREATE TABLE PersonalTrainer(
     IDNumber INTEGER PRIMARY KEY,
-    FOREIGN KEY(IDNumber),
-    REFERENCES StaffRuns(IDNumber) ON DELETE CASCADE ON UPDATE CASCADE
+    FOREIGN KEY(IDNumber) REFERENCES StaffRuns(IDNumber) ON DELETE CASCADE
 );
 CREATE TABLE ClassInstructor(
     IDNumber INTEGER PRIMARY KEY,
-    FOREIGN KEY(IDNumber),
-    REFERENCES StaffRuns(IDNumber) ON DELETE CASCADE ON UPDATE CASCADE
+    FOREIGN KEY(IDNumber) REFERENCES StaffRuns(IDNumber) ON DELETE CASCADE
 );
 CREATE TABLE LocatedIn(
     FacilityID CHAR(10),
     BranchCity CHAR(40),
-    BranchStreetAddress(60),
+    BranchStreetAddress CHAR(60),
     PRIMARY KEY(FacilityID, BranchCity, BranchStreetAddress),
-    FOREIGN KEY(FacilityID) REFERENCES Facility(IDNumber) ON DELETE CASCADE ON UPDATE CASCADE,
-    FOREIGN KEY(BranchCity, BranchStreetAddress) REFERENCES Branch(City, StreetAddress) ON DELETE CASCADE ON UPDATE CASCADE
-);
-CREATE TABLE Branch(
-    City CHAR(40),
-    StreetAddress CHAR(60),
-    PRIMARY KEY (City, StreetAddress)
+    FOREIGN KEY(FacilityID) REFERENCES Facility(IDNumber) ON DELETE CASCADE,
+    FOREIGN KEY(BranchCity, BranchStreetAddress) REFERENCES Branch(City, StreetAddress) ON DELETE CASCADE
 );
 CREATE TABLE Enrolled(
     MemberName CHAR(40),
     MemberPhone CHAR(10),
     ClassID INTEGER,
     PRIMARY KEY (MemberName, MemberPhone, ClassID),
-    FOREIGN KEY(MemberName, MemberPhone) REFERENCES Member(Name, PhoneNumber) ON DELETE CASCADE ON UPDATE CASCADE,
-    FOREIGN KEY(ClassID) REFERENCES Class(ID) ON DELETE CASCADE ON UPDATE CASCADE
+    FOREIGN KEY(MemberName, MemberPhone) REFERENCES Member(Name, PhoneNumber) ON DELETE CASCADE,
+    FOREIGN KEY(ClassID) REFERENCES Class(ID) ON DELETE CASCADE
 );
 CREATE TABLE BorrowsRentalEquipment(
     Barcode INTEGER PRIMARY KEY,
@@ -45,9 +43,9 @@ CREATE TABLE BorrowsRentalEquipment(
     MemberPhone CHAR(10),
     RentalLimit INTEGER,
     DateBorrowed DATE,
-    FOREIGN KEY(MemberName, MemberPhone) REFERENCES Member(Name, PhoneNumber) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY(MemberName, MemberPhone) REFERENCES Member(Name, PhoneNumber) ON DELETE CASCADE,
     FOREIGN KEY (RentalLimit, DateBorrowed) REFERENCES RentalEquipmentDue(RentalLimit, DateBorrowed) ON DELETE
-    SET NULL ON UPDATE CASCADE
+    SET NULL
 );
 CREATE TABLE RentalEquipmentDue(
     RentalLimit INTEGER,
@@ -59,7 +57,7 @@ CREATE TABLE Facility(
     ID CHAR(10) PRIMARY KEY,
     Size CHAR(10),
     FOREIGN KEY (Size) REFERENCES FacilitySize(Size) ON DELETE
-    SET DEFAULT ON UPDATE CASCADE
+    SET DEFAULT
 );
 CREATE TABLE FacilitySize(
     Size CHAR(10) PRIMARY KEY,
@@ -70,8 +68,8 @@ CREATE TABLE Teach(
     ClassInstructorID INTEGER,
     ClassTime CHAR(11),
     PRIMARY KEY (ClassID, ClassInstructorID),
-    FOREIGN KEY (ClassID) REFERENCES Class(ID) ON DELETE CASCADE ON UPDATE CASCADE,
-    FOREIGN KEY (ClassInstructorID) REFERENCES ClassInstructor(IDNumber) ON DELETE CASCADE ON UPDATE CASCADE
+    FOREIGN KEY (ClassID) REFERENCES Class(ID) ON DELETE CASCADE,
+    FOREIGN KEY (ClassInstructorID) REFERENCES ClassInstructor(IDNumber) ON DELETE CASCADE
 );
 CREATE TABLE Member(
     Name CHAR(40),
@@ -81,9 +79,9 @@ CREATE TABLE Member(
     MembershipTier CHAR(15) NOT NULL,
     PersonalTrainerID INTEGER,
     PRIMARY KEY(Name, PhoneNumber),
-    FOREIGN KEY (MembershipTier) REFERENCES Membership(Tier) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (MembershipTier) REFERENCES Membership(Tier) ON DELETE CASCADE,
     FOREIGN KEY (PersonalTrainerID) REFERENCES PersonalTrainer(IDNumber) ON DELETE
-    SET NULL ON UPDATE CASCADE
+    SET NULL
 );
 CREATE TABLE Membership(
     Tier CHAR(15),
@@ -97,18 +95,18 @@ CREATE TABLE PersonalBest(
     LiftType CHAR(15),
     Weight INTEGER NOT NULL,
     PRIMARY KEY (MemberName, MemberPhone, LiftType),
-    FOREIGN KEY (MemberName, MemberPhone) REFERENCES Member(Name, PhoneNumber) ON DELETE CASCADE ON UPDATE CASCADE
+    FOREIGN KEY (MemberName, MemberPhone) REFERENCES Member(Name, PhoneNumber) ON DELETE CASCADE
 );
 CREATE TABLE Accesses(
     MembershipTier CHAR(15),
     FacilityID CHAR(10),
     PRIMARY KEY (MembershipTier, FacilityID),
-    FOREIGN KEY (MembershipTier) REFERENCES Membership(Tier) ON DELETE CASCADE ON UPDATE CASCADE,
-    FOREIGN KEY (FacilityID) REFERENCES Facility (ID) ON DELETE CASCADE ON UPDATE CASCADE
+    FOREIGN KEY (MembershipTier) REFERENCES Membership(Tier) ON DELETE CASCADE,
+    FOREIGN KEY (FacilityID) REFERENCES Facility (ID) ON DELETE CASCADE
 );
 CREATE TABLE Class (
     ID INTEGER PRIMARY KEY,
     Title CHAR(40) NOT NULL,
     FacilityID CHAR(10),
-    FOREIGN KEY (FacilityID) REFERENCES Facility (ID) ON DELETE CASCADE ON UPDATE CASCADE
+    FOREIGN KEY (FacilityID) REFERENCES Facility (ID) ON DELETE CASCADE
 );
