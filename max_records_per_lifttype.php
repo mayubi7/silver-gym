20072840349429979e868e19dedf7433458e7d1c
UBC Student <amluna@students.cs.ubc.ca>
2022-11-18 02:49:26 -0800

    removing extra comments

--- max_records_per_lifttype.php
+++ max_records_per_lifttype.php
@@ -1,20 +1,7 @@
-<!--Test Oracle file for UBC CPSC304 2018 Winter Term 1
+<!--Based on Test Oracle file for UBC CPSC304 2018 Winter Term 1 which was
   Created by Jiemin Zhang
   Modified by Simona Radu
-  Modified by Jessica Wong (2018-06-22)
-  This file shows the very basics of how to execute PHP commands
-  on Oracle.
-  Specifically, it will drop a table, create a table, insert values
-  update values, and then query for values
-
-  IF YOU HAVE A TABLE CALLED "demoTable" IT WILL BE DESTROYED
-
-  The script assumes you already have a server set up
-  All OCI commands are commands to the Oracle libraries
-  To get the file to work, you must place it somewhere where your
-  Apache server can run it, and you must rename it to have a ".php"
-  extension.  You must also change the username and password on the
-  OCILogon below to be your ORACLE username and password -->
+  Modified by Jessica Wong (2018-06-22) -->
 
   <html>
 
@@ -52,14 +39,12 @@
 
     <?php
     require "util.php";
-    //this tells the system that it's no longer just parsing html; it's now parsing PHP
 
     $success = True; //keep track of errors so it redirects the page only if there are no errors
     $db_conn = NULL; // edit the login credentials in connectToDB()
     $show_debug_alert_messages = False; // set to True if you want alerts to show you which methods are being triggered (see how it is used in debugAlertMessage())
 
     // HANDLE ALL GET ROUTES
-    // A better coding practice is to have one method that reroutes your requests accordingly. It will make it easier to add/remove functionality.
     function handleGETRequest()
     {
         if (connectToDB()) {
